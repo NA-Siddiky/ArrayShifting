4d285fa5df9603d4d3915446f8106a5a3de56b74
NA-Siddiky <siddiky.academic@gmail.com>
2021-06-21 19:51:27 +0600

    Array shifting redy to submit

--- array-shifting.js
+++ array-shifting.js
@@ -1,15 +1,15 @@
-function ArrayShifting(arr, shiftingValue) {
-    console.log("Input:  ", arr)
-    let length = arr.length;
+function ArrayShifting(array, shiftingValue) {
+    console.log("Input:  ", array)
+    let length = array.length;
     for (let i = 0; i < shiftingValue; i++) {
-        let b = arr[0];
+        let b = array[0];
 
         for (let j = 0; j < length; j++) {
-            arr[j] = arr[j + 1];
+            array[j] = array[j + 1];
         }
-        arr[length - 1] = b;
+        array[length - 1] = b;
     }
-    console.log("Output: ", arr)
+    console.log("Output: ", array)
 }
 
 let InputArray = [1, 2, 3, 4, 5]
